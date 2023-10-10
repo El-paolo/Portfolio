fc574f5ba80ad39d50d8153723f5fdd02d705da4
El-paolo <paolocordovacontacto@gmail.com>
2023-10-10 16:05:38 -0300

    added cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,32 @@
-class ApplicationController < ActionController::Base before_action
-:set_photo_keys before_action :get_photo_urls protected def set_photo_keys
-@photo_keys = ['bootstrap.svg', 'github.svg', 'rails.svg', 'js.svg', 'linkedin.svg', 'postgresql.svg', '20230824_141523.jpg'] end def get_photo_urls s3 =
-Aws::S3::Resource.new( region: 'us-east-2', access_key_id:
-Rails.application.credentials.aws[:access_key_id], secret_access_key:
-Rails.application.credentials.aws[:secret_access_key] ) @photo_keys.each do
-|photo_key| object = s3.bucket('paolosportfolio').object(photo_key) url =
-object.presigned_url(:get, expires_in: 3600) photo_name =
-File.basename(photo_key, File.extname(photo_key))
-instance_variable_set("@#{photo_name}", url) end rescue
-Aws::Errors::ServiceError => e Rails.logger.error "Couldn't get photo URLs.
-Here's why: #{e.message}" end end
-
-# This function disponibilices the photos hosted in aws
+# This class makes the photos hosted on AWS available.
+class ApplicationController < ActionController::Base
+    before_action :set_photo_keys
+    before_action :get_photo_urls
+  
+    protected
+  
+    def set_photo_keys
+      @photo_keys = [
+        'bootstrap.svg', 'github.svg', 'rails.svg', 'js.svg',
+        'linkedin.svg', 'postgresql.svg', 'fotoPaolo.jpg'
+      ]
+    end
+  
+    def get_photo_urls
+      s3 = Aws::S3::Resource.new(
+        region: 'us-east-2',
+        access_key_id: Rails.application.credentials.aws[:access_key_id],
+        secret_access_key: Rails.application.credentials.aws[:secret_access_key]
+      )
+  
+      @photo_keys.each do |photo_key|
+        object = s3.bucket('paolosportfolio').object(photo_key)
+        url = object.presigned_url(:get, expires_in: 3600)
+        photo_name = File.basename(photo_key, File.extname(photo_key))
+        instance_variable_set("@#{photo_name}", url)
+      end
+    rescue Aws::Errors::ServiceError => e
+      Rails.logger.error "Couldn't get photo URLs. Here's why: #{e.message}"
+    end
+  end
+  
